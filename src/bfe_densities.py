3aacdecaef31639d0bdcf2115c63d632af75a1c3
jngaravitoc <jngaravitoc@email.arizona.edu>
2018-03-09 19:23:10 -0700

    update

--- src/bfe_densities.py
+++ src/bfe_densities.py
@@ -5,6 +5,7 @@ import sys
 import orbit
 import matplotlib.pyplot as plt
 import soda
+import matplotlib.ticker as ticker
 
 def dens_hernquist(a, r, M):
     a = a
@@ -105,7 +106,8 @@ if __name__ == "__main__":
 
 
     #coeff_path = sys.argv[1]
-    coeff_path = './coefficients/ST_MWST_MW_beta0_LMC6_6.26M_snap_0.txt'
+    #coeff_path = './coefficients/ST_MWST_MW_beta0_LMC6_6.26M_snap_0.txt'
+    coeff_path = './coefficients/ST_MWST_MW_beta0_40M_snap_0_n20_l2.txt'
     #oeff_path = './coefficients/ST_test.txt'
     #nmax = sys.argv[2]
     nmax = 20
@@ -115,19 +117,20 @@ if __name__ == "__main__":
     #r_s = sys.argv[5]
     #halo_name = sys.argv[6]
 
-    rmax = 50
+    rmax = 300
 
-    path_snap = './test_halo/LMC6_6.25M_vir_000'
+    #path_snap = './test_halo/LMC6_6.25M_vir_000'
+    path_snap = './test_halo/MW2_40M_vir_000'
     # Read coefficients
     S, T = orbit.read_coefficients(coeff_path, 1, nmax, lmax)
     #print(S, T)
     # Compute densities
-    #mass = pygadgetreader.readsnap(path_snap, 'mass', 'dm')
 
     mass = pygadgetreader.readsnap(path_snap, 'mass', 'dm')
     M_halo = np.sum(mass)
     print(M_halo)
-    r_s = 25.158
+    r_s = 40.82
+    #r_s = 25.158
     #r_s = 2.5158
     G = 43007.1
     print(S[0])
@@ -142,24 +145,51 @@ if __name__ == "__main__":
     pos_cm = re_center_halo(pos, rcm)
 
     # BFE
-    r_BFE, rho_BFE = BFE_density_profile(pos_cm, S[0], T[0], 1, r_s, rmax=100, nbins=50)
-    r_BFE, pot_BFE = BFE_potential_profile(pos_cm, S[0], T[0], 1, r_s, G, rmax=100, nbins=50)
+    r_BFE, rho_BFE = BFE_density_profile(pos_cm, S[0], T[0], 1, r_s, rmax=300, nbins=100)
+    r_BFE, pot_BFE = BFE_potential_profile(pos_cm, S[0], T[0], 1, r_s, G, rmax=300, nbins=100)
 
     # N-body
-    r_NB, rho_NB = N_body_densities(pos, mass, rmax=100, nbins=50)
-    r_NB, pot_NB = N_body_potential(pos, -pot, rmax=100, nbins=50)
+    r_NB, rho_NB = N_body_densities(pos, mass, rmax=300, nbins=100)
+    r_NB, pot_NB = N_body_potential(pos, pot, rmax=300, nbins=100)
 
     # Analytic
     pot_teo = pot_hernquist(r_s, r_NB, M_halo, G)
     rho_teo = dens_hernquist(r_s, r_NB, M_halo)
 
-    plt.loglog(r_BFE, rho_BFE, c='k')
-    plt.loglog(r_NB, rho_NB, c='C9', ls='--')
-    plt.loglog(r_NB, rho_teo, c='C1', ls=':')
+    fig, ax = plt.subplots(2, 2)
 
-    plt.savefig('bfe_density.pdf', bbox_inches='tight')
+    ax[0][0].semilogy(r_BFE, rho_BFE, c='k', label='BFE')
+    ax[0][0].semilogy(r_NB, rho_NB, c='C9', ls='--', label='Gadget')
+    ax[0][0].semilogy(r_NB, rho_teo, c='C1', ls=':', label='Analytic')
+    ax[0][0].set_ylabel(r'$\rho$')
+    ax[0][0].set_xticks(np.arange(0, 300, 50))
+
+    ax[1][0].semilogy(r_BFE,-pot_BFE, c='k')
+    ax[1][0].semilogy(r_NB, -pot_NB, c='C9', ls='--')
+    ax[1][0].semilogy(r_NB, -pot_teo, c='C1', ls=':')
+    ax[1][0].set_ylabel(r'$\phi$')
+    ax[1][0].set_xlabel(r'$r [kpc]$')
+    ax[1][0].set_xticks(np.arange(0, 300, 50))
+
+    ax[0][1].plot(r_BFE, rho_BFE/rho_teo, c='k', label='BFE/Analytic')
+    ax[0][1].plot(r_NB, rho_NB/rho_teo, c='C9', ls='--', label='Gadget/Analytic')
+    ax[0][1].plot(r_NB, rho_BFE/rho_NB, c='C1', ls=':', label='BFE/Gadget')
+    ax[0][1].set_ylabel(r'$\Delta \rho$')
+    ax[0][1].set_xticks(np.arange(0, 300, 50))
+
+    ax[1][1].plot(r_BFE, pot_BFE/pot_teo, c='k')
+    ax[1][1].plot(r_NB, pot_NB/pot_teo, c='C9', ls='--')
+    ax[1][1].plot(r_NB, pot_BFE/pot_NB, c='C1', ls=':')
+    ax[1][1].set_ylabel(r'$\Delta \phi$')
+    ax[1][1].set_xlabel(r'$r [kpc]$')
+    ax[1][1].set_xticks(np.arange(0, 300, 50))
+
+    ax[0][0].legend(fontsize=15)
+    ax[0][1].legend(fontsize=15)
+
+    plt.savefig('bfe_density_MW_40M.pdf', bbox_inches='tight')
+
     # COM
     # truncate halo
 
     # plot
-    
